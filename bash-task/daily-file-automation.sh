bc96636a4a08a210e8159f867bff057246b3f570
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-07-28 11:15:07 +0700

    Update daily-file-automation.sh
     add another condition if directory exists inside the path

--- bash-task/daily-file-automation.sh
+++ bash-task/daily-file-automation.sh
@@ -1,9 +1,13 @@
 path=/hdfs/data/data1
 name_of_directory=$data1
 dir=$path/$name_of_directory
+filename_excel=daily_market_price.xlsx
+source_dir=/local/data/market
 
 if [ -d $dir ]; then
     echo "There is ${name_of_directory} Directory Exist!"
+    cp $source_dir/$filename_excel $path
+    echo "File Moved Succesfully"
 else
     echo " ${name_of_directory} Directory Not Exist"
     mkdir $dir
